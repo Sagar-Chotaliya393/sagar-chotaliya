7bb234ad32c7a9cbe571552bc203508b8d4c6c42
Sagar-Chotaliya393 <schotaliya52@gmail.com>
2026-07-01 21:23:19 +0530

    loader

--- src/component/preloader/Preloader.jsx
+++ src/component/preloader/Preloader.jsx
@@ -19,15 +19,28 @@ function Preloader() {
 
   return (
     <div className={`preloader ${fadeOut ? "fade-out" : ""}`}>
+      {/* Decorative background blobs to match hero gradient */}
+      <div className="preloader-blob preloader-blob--top" />
+      <div className="preloader-blob preloader-blob--bottom" />
+
       <div className="preloader-content">
         <div className="logo-wrapper">
+          <div className="logo-glow" />
           <img src={logo} alt="Sagar.dev" className="preloader-logo" />
-          <div className="logo-ring"></div>
+          <div className="logo-ring" />
         </div>
+
         <h2 className="preloader-text">
           Sagar<span>.dev</span>
         </h2>
+
         <p className="preloader-subtext">Loading experience...</p>
+
+        <div className="preloader-dots">
+          <span />
+          <span />
+          <span />
+        </div>
       </div>
     </div>
   );
